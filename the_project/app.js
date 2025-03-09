2278af818a566be411413e306fa6417038f8d183
Martin Poulsen <mapo0004@stud.kea.dk>
2025-03-09 13:25:46 +0100

    added html pages and css
    added a main css file, a navbar css file
    a footer.js
    
    and html pages for each week

---
+++ the_project/app.js
@@ -0,0 +1,44 @@
+// Importing express
+import express from 'express';
+
+// Create an instanse of express    
+const app = express();
+
+// Tells express to use the public folder to serve static files
+app.use(express.static("public"));  
+
+// Importing path module
+import path from 'path';
+
+// Setting up the route for the frontpage of the website
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+});
+
+app.get("/weekone", (req, res) => {
+    res.sendFile(path.resolve('public/week/weekone.html'));
+});
+
+app.get("/weektwo", (req, res) => {
+    res.sendFile(path.resolve('public/week/weektwo.html'));
+});
+
+app.get("/weekthree", (req, res) => {
+    res.sendFile(path.resolve('public/week/weekthree.html'));
+});
+
+app.get("/weekfour", (req, res) => {
+    res.sendFile(path.resolve('public/week/weekfour.html'));
+});
+
+app.get("/weekfive", (req, res) => {
+    res.sendFile(path.resolve('public/week/weekfive.html'));
+});
+
+app.get("/weeksix", (req, res) => {
+    res.sendFile(path.resolve('public/week/weeksix.html'));
+});
+
+const PORT = Number(process.env.PORT) || 8080;
+// app.listen() takes two arguments, the port number and a callback function
+const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));
