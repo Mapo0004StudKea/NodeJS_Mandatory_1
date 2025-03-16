aff691e6ac29976a3d626e0ec23f40f55fc192bb
Martin Poulsen <mapo0004@stud.kea.dk>
2025-03-16 12:47:45 +0100

    added new button

---
+++ the_project/public/assets/js/button.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tilbageKnap = document.getElementById("tilbageKnap");
+    const week1Knap = document.getElementById("week1Knap");
+    const week2Knap = document.getElementById("week2Knap");
+    const week3Knap = document.getElementById("week3Knap");
+    const week4Knap = document.getElementById("week4Knap");
+    const week5Knap = document.getElementById("week5Knap");
+    const week6Knap = document.getElementById("week6Knap");
+  
+    if (tilbageKnap) {
+      tilbageKnap.addEventListener("click", function() {
+        window.location.href = "/";
+      });
+    }
+  
+    if (week1Knap) {
+      week1Knap.addEventListener("click", function() {
+        window.location.href = "/week/1";
+      });
+    }
+    if (week2Knap) {
+        week2Knap.addEventListener("click", function() {
+          window.location.href = "/week/2";
+        });
+      }
+    if  (week3Knap) {
+        week3Knap.addEventListener("click", function() {
+            window.location.href = "/week/3";
+          });
+    }
+    if  (week4Knap) {
+        week4Knap.addEventListener("click", function() {
+            window.location.href = "/week/4";
+          });
+    }
+    if  (week5Knap) {
+        week5Knap.addEventListener("click", function() {
+            window.location.href = "/week/5";
+          });
+    }
+    if  (week6Knap) {
+        week6Knap.addEventListener("click", function() {
+            window.location.href = "/week/6";
+          });
+    }
+  });
